1b7e32c0c934555baeca20f398509442577fa5bf
Mehdi Oueldi <mehdi.oueldi@be-mo.io>
2021-05-26 09:10:44 +0200

    added di extensions

--- EntityTable/Extensions/ServiceCollectionExtensions.cs
+++ EntityTable/Extensions/ServiceCollectionExtensions.cs
@@ -8,28 +8,46 @@ namespace EntityTableService.Extensions
     /// </summary>
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddScopedEntityTableClient<T>(this IServiceCollection services,
+
+            EntityTableClientOptions tableClientOptions,
+            EntityTableConfig<T> tableClientConfig)
+            where T : class, new()
+        {
+
+            return services.AddScoped<IEntityTableClient<T>>(_ => new EntityTableClient<T>(tableClientOptions, tableClientConfig));
+        }
         public static IServiceCollection AddEntityTableClient<T>(this IServiceCollection services,
-            string connectionString,
+
+       EntityTableClientOptions tableClientOptions,
+       EntityTableConfig<T> tableClientConfig)
+       where T : class, new()
+        {
+
+            return services.AddTransient<IEntityTableClient<T>>(_ => new EntityTableClient<T>(tableClientOptions, tableClientConfig));
+        }
+
+        public static IServiceCollection AddEntityTableClient<T>(this IServiceCollection services,          
             Action<EntityTableClientOptions> tableClientOptions,
             Action<EntityTableConfig<T>> tableClientConfig)
             where T : class, new()
         {
-            var options = new EntityTableClientOptions(connectionString, typeof(T).Name, maxConcurrentInsertionTasks: 1);
+            var options = new EntityTableClientOptions(string.Empty, typeof(T).Name, maxConcurrentInsertionTasks: 1);
             var config = new EntityTableConfig<T>();
 
             tableClientOptions?.Invoke(options);
             tableClientConfig?.Invoke(config);
 
-            return services.AddTransient<IEntityTableClient<T>>(_ => new EntityTableClient<T>(options, config));
+            return services.AddEntityTableClient<IEntityTableClient<T>>(options, config);
         }
-
+       
         public static IServiceCollection AddScopedEntityTableClient<T>(this IServiceCollection services,
-           string connectionString,
+         
            Action<EntityTableClientOptions> tableClientOptions,
            Action<EntityTableConfig<T>> tableClientConfig)
            where T : class, new()
         {
-            var options = new EntityTableClientOptions(connectionString, typeof(T).Name, maxConcurrentInsertionTasks: 1);
+            var options = new EntityTableClientOptions(string.Empty, typeof(T).Name, maxConcurrentInsertionTasks: 1);
             var config = new EntityTableConfig<T>();
 
             tableClientOptions?.Invoke(options);
